27a736a05a5c4f7baf01de7d6200b966e2ddc7ab
Adrian_Bronowski <229849@edu.p.lodz.pl>
2022-06-09 21:06:01 +0200

    Nowe DataAPI

--- Projekt_AB_MD/Dane/Ball.cs
+++ Projekt_AB_MD/Dane/Ball.cs
@@ -3,64 +3,87 @@ using System.Runtime.CompilerServices;
 
 namespace Dane
 {
-    internal class Ball : Data
-    {
-        private readonly int ID = 0;
-        public int X;
-        public int Y;
-        public double Velocity;
-        private readonly double Mass = 0.0;
-
-        public override event PropertyChangedEventHandler? PropertyChanged;
-
-        public Ball(int ID, int X, int Y, double Mass, double Velocity)
+    public abstract class Obiekt {
+        public static Obiekt newBall(int ID, int X, int Y, double Mass, double Velocity)
         {
-            this.ID = ID;
-            this.X = X;
-            this.Y = Y;
-            this.Mass = Mass;
-            this.Velocity = Velocity;
+            return new Ball(ID, X, Y, Mass, Velocity);
         }
 
-        public override int objectID
-        {
-            get { return ID; }
-        }
+        public abstract void objectMove();
+        public abstract int objectID { get; }
+        public abstract double objectX { get; set; }
+        public abstract double objectY { get; set; }
+        public abstract double objectVelocity { get; set; }
+        public abstract double objectMass { get; }
 
-        public override int objectX
+        public abstract event PropertyChangedEventHandler? PropertyChanged;
+        internal class Ball : Obiekt, INotifyPropertyChanged
         {
-            get { return X; }
-            set
+            private readonly int ID = 0;
+            public double X;
+            public double Y;
+            public double Velocity;
+            private readonly double Mass = 0.0;
+
+            public override event PropertyChangedEventHandler? PropertyChanged;
+
+            public Ball(int ID, int X, int Y, double Mass, double Velocity)
             {
-                X = value;
-                OnPropertyChanged();
+                this.ID = ID;
+                this.X = X;
+                this.Y = Y;
+                this.Mass = Mass;
+                this.Velocity = Velocity;
             }
-        }
-
-        public override int objectY
-        {
-            get { return Y; }
-            set
+            public override void objectMove()
             {
-                Y = value;
-                OnPropertyChanged();
+                X += Velocity;
+                Y += Velocity;
             }
-        }
 
-        public override double objectVelocity
-        {
-            get { return Velocity; }
-            set { Velocity = value; }
-        }
+            public override int objectID
+            {
+                get => ID; 
+            }
 
-        public override double objectMass
-        {
-            get { return Mass; }
-        }
+            public override double objectX
+            {
+                get => X;
+                set
+                {
+                    X = value;
+                    OnPropertyChanged();
+                }
+            }
 
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            public override double objectY
+            {
+                get => Y;
+                set
+                {
+                    Y = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public override double objectVelocity
+            {
+                get => Velocity;
+                set 
+                { 
+                    Velocity = value; 
+                }
+            }
+
+            public override double objectMass
+            {
+                get => Mass;
+            }
+
+            protected void OnPropertyChanged([CallerMemberName] string name = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+            }
         }
 
     }
--- Projekt_AB_MD/Dane/Dane.cs
+++ Projekt_AB_MD/Dane/Dane.cs
@@ -6,6 +6,16 @@ namespace Dane
     public abstract class Data : INotifyPropertyChanged
     {
         // Klasa abstrakcyjna umożliwiająca stworzenie własnych funkcji zajmujących się daną strukturą
+        public static List<Obiekt> Create()
+        {
+            return new List<Obiekt>;
+        }
+
+        public static Obiekt newBall(int ID, int X, int Y, double Mass, double Velocity)
+        {
+            return new Ball(ID, X, Y, Mass, Velocity);
+        }
+
         public abstract int objectID { get; }
         public abstract int objectX { get; set; }
         public abstract int objectY {get; set; }
